02b7433934d8b495f688a81b31c3b89474ae42e1
MrVenomWolf <86920461+MrVenomWolf@users.noreply.github.com>
2022-08-26 11:03:33 -0400

    Update perfectnumber.java

--- Hw2/perfectnumber.java
+++ Hw2/perfectnumber.java
@@ -1,4 +1,10 @@
 // Anubrota Majumdar, 114586887
+/*A positive integer is called a perfect number if it is equal to the sum of all of its positive divisors, excluding itself. For example, 6 is the first perfect number because 6 = 3 + 2 + 1. The next is 28 = 14 + 7 + 4 + 2 +1.
+Write a program that asks the user for an upper limit and prints all the perfect numbers up to that limit.
+Example:
+Enter the upper limit: 10000
+The perfect numbers below 10000 are: 6 28 496 8128.
+*/
 import java.util.Scanner;
  
 public class perfectnumber{
