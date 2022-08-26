edd767bf2e66e6e395c03c03e9ff432166590b19
MrVenomWolf <86920461+MrVenomWolf@users.noreply.github.com>
2022-08-26 11:05:41 -0400

    Update palindrome.java

--- Hw2/palindrome.java
+++ Hw2/palindrome.java
@@ -1,4 +1,15 @@
 // Anubrota Majumdar, 114586887
+/*
+Write a method that checks whether the input string or a sentence (a string with spaces) is a palindrome or not. The method should be case insensitive and should ignore spaces. Write a test program that prompts the user to input a string and invokes this method. Some example runs are:
+Enter the input string: madam
+Input string madam is a palindrome
+Enter the input string: banana
+Input string banana is NOT a palindrome
+Enter the input string: Race Car
+Input string Race Car is a palindrome
+Enter the input string: Too HOT to hoot
+Input string Too HOT to hoot is a palindrome
+*/
 import java.util.Scanner;
 public class palindrome {
 
