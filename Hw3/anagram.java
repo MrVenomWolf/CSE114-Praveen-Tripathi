38d3c40cef1a05a5b0e88000a43965415a90211e
MrVenomWolf <86920461+MrVenomWolf@users.noreply.github.com>
2022-08-26 11:10:13 -0400

    Update anagram.java

--- Hw3/anagram.java
+++ Hw3/anagram.java
@@ -1,3 +1,15 @@
+/*
+Two strings are anagrams if they are written using the same exact letters. Write a method to check if given two strings are anagrams or not. You have to ignore the case and space characters. Write a test program for that prompts the user to input two strings and invokes this method. Some example runs are:
+Enter the first string: abbacba
+Enter the second string: abcabba
+abbacba and abcabba are anagrams
+Enter the first string: banana
+Enter the second string: cabana
+banana and cabana are NOT anagrams
+Enter the first string: Eleven plus two
+Enter the second string: Twelve plus one
+Eleven plus two and Twelve plus one are anagrams
+*/
 import java.util.Scanner;
 public class anagram {
 
